9fbadbae537a6c46847ce73e86bf6ea5ca03b600
ekherman <eherman@ualberta.ca>
2020-06-11 14:08:08 -0600

    update pip install

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SNP Conversion",
+    name="snp_conversion_ekherman",
+    author="Emily Herman",
+    description="Tools for manipulating and converting SNP information",
+    url="https://github.com/stothard-group/snp_conversion",
     version="2.0",
     packages=find_packages(include=['lib/*']),
     install_requires=["numpy", "pandas", "python-dateutil", "scikit-allel", "toolz"],
-    python_requires='>=3',
-
-
+    python_requires='>=3.6',
+    classifiers=["Programming Language :: Python :: 3",
+                 "Operating System :: Unix",
+                 "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
 
 )
