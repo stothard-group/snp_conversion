c2910567638927741c563940e215b1df2825db17
ekherman <49324081+ekherman@users.noreply.github.com>
2021-02-28 15:43:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Emily Herman",
     description="Tools for manipulating and converting SNP information",
     url="https://github.com/stothard-group/snp_conversion",
-    version="2.0",
+    version="3.0",
     packages=find_packages(include=['lib/*']),
     install_requires=["numpy", "pandas", "python-dateutil", "dask==2.14.0", "scikit-allel", "toolz"],
     python_requires='>=3.6',
